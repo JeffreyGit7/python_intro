847aa3dfa7a96260bb614e98e9287484bf64473d
Jeffrey <jeffrey.odawa@strathmore.edu>
2022-10-26 19:56:31 +0300

    python_files v1

---
+++ 1_output.py
@@ -0,0 +1,8 @@
+# Output in python
+# The print function is used to display on the screen
+print("Hello World")
+
+# python statements have no terminator
+print(2+2)
+
+print(10%3)
---
+++ 2_datatypes.py
@@ -0,0 +1,30 @@
+# Datatypes in python
+
+# Primitive datatypes
+
+from operator import truediv
+
+
+x = 5  # Int data type
+y = 2.5  # Float data type
+
+# Python allows variable declaraations on the same line
+# x, y = 5, 2.5
+
+# Python allows setting multiple variables to the same value
+a = b = c = 3
+
+my_string = ("Hello There")  # str data type
+is_true = True  # Boolean data type
+is_false = False  # Boolean data type
+my_string = [1, 2, 3]  # List data type
+none_type = None
+my_char = ('a')
+
+print(type(x))
+print(type(y))
+print(type(a))
+print(type(my_string))
+print(type(is_true))
+print(type(none_type))
+print(type(my_char))
